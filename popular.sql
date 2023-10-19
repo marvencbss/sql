ca89980bd129c1e0bbbcdf4db841ab5ace63f9a1
marvencbss <130517545+marvencbss@users.noreply.github.com>
2023-10-19 20:27:37 -0300

    .

--- popular.sql
+++ popular.sql
@@ -1,16 +1,7 @@
-INSERT INTO curso VALUES 
-(1,'programação front end','MANHÃ', 'D04 108'),
-(2,'programação front end','TARDE', 'D04 108'),
-(3,'programação front end','NOITE', 'D04 108'),
-(4,'programação back end','MANHÃ', 'D04 108'),
-(5,'programação back end','TARDE', 'D04 108'),
-(6,'programação back end','NOITE', 'D04 108');
-
-
-INSERT INTO aluno VALUES
-(1,'Luis',2),
-(2,'Andre',6),
-(3,'Marven',4),
-(4,'Luisa',1),
-(5,'Izabela',2),
-(6,'Fernanda',3);
+INSERT INTO curso VALUES
+(1,'Programação Backend','NOITE','D04 108'),
+(2,'Programação Backend','TARDE','D04 108'),
+(3,'Programação Backend','MANHÃ','D04 108'),
+(4,'Programação Front','NOITE','D04 108'),
+(5,'Programação Front','TARDE','D04 108'),
+(6,'Programação Front','MANHÃ','D04 108');
