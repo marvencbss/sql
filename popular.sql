c9f926a1c30b038a87908aa13c88c6a89418758d
marvencbss <130517545+marvencbss@users.noreply.github.com>
2023-10-19 21:08:20 -0300

    Delete popular.sql

--- popular.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO curso VALUES
-(1,'Programação Backend','NOITE','D04 108'),
-(2,'Programação Backend','TARDE','D04 108'),
-(3,'Programação Backend','MANHÃ','D04 108'),
-(4,'Programação Front','NOITE','D04 108'),
-(5,'Programação Front','TARDE','D04 108'),
-(6,'Programação Front','MANHÃ','D04 108');
