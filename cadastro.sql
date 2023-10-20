510348cc267627c54109c9c61766685f83e21016
marvencbss <130517545+marvencbss@users.noreply.github.com>
2023-10-19 21:07:17 -0300

    .

---
+++ cadastro.sql
@@ -0,0 +1,23 @@
+INSERT INTO aluno VALUES
+(1,'Gustavo',1),
+(2,'Simone',4),
+(3,'Mauricio',6),
+(4,'Luan',2),
+(5,'Andressa',3),
+(6,'Marven',5);
+
+INSERT INTO professor VALUES 
+(1, 'Gustavo', 1),
+(2, 'Andre', 2),
+(3, 'Josias', 4),
+(4, 'Jonei', 5),
+(5, 'Juao', 6),
+(6, 'Cleide', 3);
+
+INSERT INTO curso VALUES
+(1,'Programação Backend','NOITE','D04 108'),
+(2,'Programação Backend','TARDE','D04 108'),
+(3,'Programação Backend','MANHÃ','D04 108'),
+(4,'Programação Front','NOITE','D04 108'),
+(5,'Programação Front','TARDE','D04 108'),
+(6,'Programação Front','MANHÃ','D04 108');
